ec18edde6671edceb8a15fc93bd8e8d8348d6bb2
Lucifer378 <157404444+Lucifer378@users.noreply.github.com>
2024-01-26 05:10:44 +0700

    Update Run.py

--- Run.py
+++ Run.py
@@ -57,7 +57,7 @@ for z in range(200):
 	versi = random.choice(["10_0_2","10_1_1","10_2","10_2_1","10_3_1","10_3_2","10_3_3"])
 	verchrome = random.choice(["602.1.50","602.2.14","602.3.12","602.4.6","603.1.30","603.2.4","603.3.8","601.1.46"])
 	mob = random.choice(["14A456","14B100","14C92","14D27","14E304","14F89","14G60","13C75","13D15","13E233","13E238","13F69","13G34","13G36"])
-	ua = f"Dalvik/2.1.0 (Linux; Android {android}; WOW64) [FBAN/MessengerLite;FBAV/{versi_chrome};FBPN/com.facebook.mlite;FBLC/de_DE;FBRV/{str(rr(111111111,999999999))};FBCR/Airtel;FBMF/Facebook;Facebook/Ige;FBDV/WOW64;FBSV/{android};FBCA]"
+	ua = f"Mozilla/5.0 (iPhone; CPU iPhone OS 17_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/21B80 [FBAN/FBIOS;FBDV/iPhone16,2;FBMD/iPhone;FBSN/iOS;FBSV/17.1;FBSS/3;FBID/phone;FBLC/vi_VN;FBOP/5]"
 	if ua in ugent:pass
 	else:ugent.append(ua)
 		
