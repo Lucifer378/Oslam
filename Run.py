60de9154c156d1206d51e4e25839b7ab6be3cd0c
Lucifer378 <157404444+Lucifer378@users.noreply.github.com>
2024-02-13 09:17:59 +0700

    Update Run.py

--- Run.py
+++ Run.py
@@ -58,7 +58,7 @@ for z in range(200):
 	versi = random.choice(["10_0_2","10_1_1","10_2","10_2_1","10_3_1","10_3_2","10_3_3"])
 	verchrome = random.choice(["602.1.50","602.2.14","602.3.12","602.4.6","603.1.30","603.2.4","603.3.8","601.1.46"])
 	mob = random.choice(["14A456","14B100","14C92","14D27","14E304","14F89","14G60","13C75","13D15","13E233","13E238","13F69","13G34","13G36"])
-	ua = f"Dalvik/2.1.0 (Windows NT {android}; Win64; x64) [FBAN/FB4A;FBAV/{versi_chrome};FBBV/{str(rr(111111111,999999999))};FBRV/0;FBLC/pt_BR;FBMF/Samsung;FBBD/Samsung;FBDV/Win64; x64;FBSV/{android};FBCA]"
+	ua = f"Mozilla/5.0 (Linux; Android {android}; en-US; SM-J105F Build/{build}; wv) [FBAN/FB4A;FBAV/{versi_chrome};FBBV/{str(rr(111111111,999999999))};FBRV/0;FBLC/pt_BR;FBMF/Samsung;FBBD/Samsung;FBDV/SM-J105F;FBSV/{android};FBCA]"
 	if ua in ugent:pass
 	else:ugent.append(ua)
 		
